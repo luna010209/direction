d674fe2fd63771e54f770dc92e9203dd06612dd1
Lunado <liendhhha140217@gmail.com>
2025-02-11 23:51:30 +0900

    Setup dependencies

---
+++ src/main/java/com/example/direction/DirectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.direction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/direction/DirectionController.java
@@ -0,0 +1,7 @@
+package com.example.direction;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DirectionController {
+}
